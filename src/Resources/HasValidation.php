91695e7159cb994070b80017fbabbdda5eb74013
Samuel Pacheco <samuelpachecopst@gmail.com>
2023-09-16 15:37:32 -0300

    fix validation

--- src/Resources/HasValidation.php
+++ src/Resources/HasValidation.php
@@ -4,16 +4,24 @@ namespace S4mpp\AdminPanel\Resources;
 
 use Illuminate\Http\Request;
 use Illuminate\Validation\Rule;
+use Illuminate\Support\ValidatedInput;
 use Illuminate\Support\Facades\Validator;
 
 trait HasValidation
 {
-	private static function _validate($resource, Request $request, array $fields, int $id = null)
+	private static function _validate($resource, Request $request, array $fields, int $id = null): ValidatedInput
 	{
 		$validation_rules = $attributes = [];
 
+		$input = $request->input();
+
 		foreach($fields as $field)
 		{
+			if($field->prepare_for_validation)
+			{
+				$input[$field->name] = call_user_func($field->prepare_for_validation, $input[$field->name]);
+			}
+
 			$rules = [];
 			foreach($field->rules as $rule)
 			{
@@ -33,8 +41,10 @@ trait HasValidation
 			$attributes[$field->name] = $field->title;
 		}
 
-		$validator = Validator::make($request->input(), $validation_rules, [], $attributes);
+		$validator = Validator::make($input, $validation_rules, [], $attributes);
 
 		$validator->validate();
+
+		return $validator->safe();
 	}
 }
