f0eddd24a750a0a1ab5d036b6355545b3aa126c4
Samuel Pacheco <samuelpachecopst@gmail.com>
2023-09-20 17:34:50 -0300

    remove install command

--- src/Providers/AdminPanelServiceProvider.php
+++ src/Providers/AdminPanelServiceProvider.php
@@ -4,7 +4,6 @@ namespace S4mpp\AdminPanel\Providers;
 
 use Livewire\Livewire;
 use Illuminate\Pagination\Paginator;
-use S4mpp\AdminPanel\Commands\Install;
 use Illuminate\Support\ServiceProvider;
 use S4mpp\AdminPanel\Resources\Resource;
 use S4mpp\AdminPanel\Commands\CreateAdmin;
@@ -36,8 +35,7 @@ class AdminPanelServiceProvider extends ServiceProvider
 		{
 			$this->commands([
 				CreateAdmin::class,
-				ResetPasswordAdmin::class,
-				Install::class,
+				ResetPasswordAdmin::class
 			]);
 
 			$this->publishes([
