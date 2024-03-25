c30b9dc88648d20dcaa016e07be370013390436e
samuel pacheco <samuelpachecopst@gmail.com>
2024-03-25 01:19:24 -0300

    ajustes

--- workbench/config/admin.php
+++ workbench/config/admin.php
@@ -5,8 +5,6 @@ use function Orchestra\Testbench\workbench_path;
 return [
 	'guard' => 'web',
 	
-	'prefix' => 'painel',
-
 	'resources_path' => workbench_path('app/AdminPanel'),
 	
 	'namespace' => 'Workbench\\App',
